cfcef4e3c9b7c55506babb12972c9531e4d0bc3b
yrberrytea <yrberrytea@gmail.com>
2021-01-04 23:37:07 +0900

    解答例を見て改良

---
+++ training/N-yobikou/Large-Web-Application/Basic-Scala/scala-study/BMSearch_1.scala
@@ -0,0 +1,44 @@
+//解答例のコードを見てスキップテーブル作成部分の書き方を取り込み
+
+object BMSearch_1 extends App {
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndexes = search(text, pattern)
+
+  def search(text: Seq[Char], pattern: Seq[Char]): Seq[Int] = {
+    val skipTable: Map[Char, Int] = getSkipNums(pattern)  //文字をキー、その文字が出現した際にずらす数を値に持つ連想配列
+    val lastIndex = text.length - pattern.length
+    var matchIndexes = Seq[Int]()
+    var i = 0
+
+    while (i <= lastIndex) {
+      val partText = text.slice(i, i + pattern.length)
+      val (isMatch, shift) = matchTextAndGetNumToShift(partText, pattern, skipTable)  //isMatch: Boolean, shift: Int
+      if (isMatch) matchIndexes = matchIndexes :+ i
+      i = i + shift
+    }
+
+    matchIndexes
+  }
+
+  def getSkipNums(pattern: Seq[Char]): Map[Char, Int] = {
+    return pattern.map(s => (s -> (pattern.reverse.indexOf(s)))).toMap
+  }
+
+  def matchTextAndGetNumToShift(partText: Seq[Char], pattern: Seq[Char], skipTable: Map[Char, Int]): (Boolean, Int) = {
+    var isMatch = false
+    var shift = pattern.length
+
+    for (i <- pattern.length - 1 to 0 by -1) {
+      if (partText(i) != pattern(i)) {
+        shift = skipTable.getOrElse(partText(i), shift)
+        return (isMatch, shift)
+      }
+    }
+    isMatch = true
+
+    (isMatch, shift)
+  }
+
+  println(s"出現場所: ${matchIndexes}")
+}
