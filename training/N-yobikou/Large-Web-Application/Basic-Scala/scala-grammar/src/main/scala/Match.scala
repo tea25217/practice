2fa04f0f33a319f550ec2f9be45f56f0711b9c7b
yrberrytea <yrberrytea@gmail.com>
2021-02-09 05:43:27 +0900

    Study match expression in Scala

---
+++ training/N-yobikou/Large-Web-Application/Basic-Scala/scala-grammar/src/main/scala/Match.scala
@@ -0,0 +1,80 @@
+object Match {
+
+  def intToName(num: Int): String = {
+    num match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "other"
+    }
+  }
+
+  def message(message: String): String = {
+    message match {
+      case "good" | "bad" => "game"
+    }
+  }
+
+  def patternMatch(): Unit = {
+    val seq = Seq("A", "B", "C", "D", "E")
+    seq match {
+      case Seq("A", b, c, d, e) if b != "B" =>
+        println("b = " + b)
+        println("c = " + c)
+        println("d = " + d)
+        println("e = " + e)
+      case _ =>
+        println("nothing")
+    }
+  }
+
+  def patternMatchNest(): Unit = {
+    val seq = Seq(Seq("A"), Seq("B", "C", "D", "E"))
+    seq match {
+      case Seq(a@Seq("A"), x) =>
+        println(a)
+        println(x)
+      case _ => println("nothing")
+    }
+  }
+
+  def patternMatchType(): Unit = {
+    val obj: AnyRef = "String Literal"
+    obj match {
+      case v: java.lang.Integer => println("Integer!")
+      case v: String => println(s"String! length is ${v.length}")
+    }
+  }
+
+  def patternMatchGenerics(): Unit = {
+    val obj: AnyRef = Seq("a", "b", "c")
+    obj match {
+      case v: Seq[_] => println("Seq[_]")
+      case _ => println("nothing")
+    }
+  }
+
+  def printRandomChars(): Unit = {
+    for (i <- 1 to 1000) {
+      val chars: Seq[Char] = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList
+      val result = chars match {
+        case Seq(a, b, c, d, _) => Seq(a, b, c, d, a)
+      }
+      println(result)
+    }
+  }
+
+  def last(seq: Seq[Int]): Int = {
+    seq match {
+      case x::xs if xs.length >= 2 => last(xs)
+      case x::xs if xs.length == 1 => xs(0)
+      case x => x(0)
+    }
+  }
+
+  def reverse(seq: Seq[Int]): Seq[Int] = {
+    seq match {
+      case x if x.length == 1 => x
+      case x::xs => reverse(xs) :+ x
+    }
+  }
+}
