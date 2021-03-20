a993d5cb4fc9522257dbb78788212b13adb10240
yrberrytea <yrberrytea@gmail.com>
2021-03-20 17:45:21 +0900

    DPの練習

--- atcoder/EducationalDPContest/b.py
+++ atcoder/EducationalDPContest/b.py
@@ -1,4 +1,4 @@
-INF = 10 ** 5
+INF = 10 ** 9
 
 n, k = map(int, input().split())
 h = list(map(int, input().split())) + [INF] * (k)
@@ -9,8 +9,6 @@ dp[0] = 0
 for i in range(n):
     for j in range(1, k + 1):
         cost = abs(h[i] - h[i + j])
-        if (dp[i] + cost) >= dp[i + j]:
-            continue
-        dp[i + j] = dp[i] + cost
+        dp[i + j] = min(dp[i + j], dp[i] + cost)
 
 print(dp[n - 1])
