c9f3ad0bf7d3a13c2450068585d9e45966e4f968
yrberrytea <yrberrytea@gmail.com>
2021-04-22 16:18:44 +0900

    study Fork/Join framework

---
+++ training/N-yobikou/Large-Web-Application/Concurrent-Programming/scala-concurrent/src/main/scala/SumTrial.scala
@@ -0,0 +1,12 @@
+object SumTrial extends App {
+
+  val length = 10000000
+  val list = (for (i <- 1 to length) yield i.toLong).toList
+
+  val start = System.currentTimeMillis()
+  val sum = list.sum
+  val time = System.currentTimeMillis() - start
+
+  println(sum)
+  println(s"time: ${time} msec")
+}
