f66724f43500ae89e5cbfeed5220ab8780ae0704
yrberrytea <yrberrytea@gmail.com>
2021-07-17 22:08:56 +0900

    try abc210

---
+++ competitive-programming/atcoder/abc210/b.py
@@ -0,0 +1,10 @@
+n = int(input())
+s = input()
+
+for i in range(n):
+    if s[i] == '1':
+        if i % 2 == 0:
+            print('Takahashi')
+        else:
+            print('Aoki')
+        exit()
