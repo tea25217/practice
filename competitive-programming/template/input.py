66956ae13268c348305b2238eb1a5fdedc50d6c3
yrberrytea <yrberrytea@gmail.com>
2022-02-10 18:06:17 +0900

    update template

--- competitive-programming/template/input.py
+++ competitive-programming/template/input.py
@@ -45,3 +45,30 @@ def toInverseElement(M, n):
         return ans
 
     return modPow(n, M - 2, M)
+
+
+# NumPyでbit全探索する時のテンプレ
+
+# (N以下の整数の中でV1..Vkいずれかの倍数であるものの個数を出力するプログラム)
+# 以下をbit全探索
+# V[j]の最小公倍数を出す。
+# 立ってるbitの本数が奇数ならaccに足す。偶数なら引く。
+import numpy as np
+
+n, k = map(int, input().split())
+v = np.array(list(map(int, input().split())))
+acc = 0
+
+for i in range(2 ** k):
+    mask = np.array(list(map(lambda x: bool(int(x)), bin(i)[2:].zfill(k))))
+    picked = v[mask]
+    if len(picked) == 0:
+        continue
+    lcm_v = np.lcm.reduce(picked)
+    num = n // lcm_v
+    if len(picked) % 2 == 1:
+        acc += num
+    else:
+        acc -= num
+
+print(acc)
