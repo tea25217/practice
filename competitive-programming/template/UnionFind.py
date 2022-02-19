f36bbcb46a48a6f9c4bf82952274401a0d413508
yrberrytea <yrberrytea@gmail.com>
2022-02-19 19:43:14 +0900

    update docstring

--- competitive-programming/template/UnionFind.py
+++ competitive-programming/template/UnionFind.py
@@ -1,10 +1,26 @@
 class UnionFind():
+    """Union Find Tree
+
+    Attributes:
+        n (int): num of nodes
+        par (list[int]): parent of nodes
+        rank (list[int]): depth of connected subtrees
+
+    """
     def __init__(self, n: int) -> None:
         self.n = n
         self.par = [i for i in range(n + 1)]
         self.rank = [0] * (n + 1)
 
     def find(self, x: int) -> int:
+        """Find root node of x
+
+        Args:
+            x (int): node
+
+        Returns:
+            int: root node of x
+        """
         if self.par[x] == x:
             return x
         else:
@@ -12,6 +28,12 @@ class UnionFind():
             return self.par[x]
 
     def unite(self, x: int, y: int) -> None:
+        """Connect subtrees include node x and node y
+
+        Args:
+            x (int): node
+            y (int): node
+        """
         x_root = self.find(x)
         y_root = self.find(y)
 
@@ -29,4 +51,13 @@ class UnionFind():
                 self.rank[y_root] += 1
 
     def same(self, x: int, y: int) -> bool:
+        """ Node x and y are in same subtree or not
+
+        Args:
+            x (int): node
+            y (int): node
+
+        Returns:
+            bool: True if x and y are in same subtree. False otherwise.
+        """
         return self.find(x) == self.find(y)
