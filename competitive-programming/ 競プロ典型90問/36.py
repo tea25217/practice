068f784dab0f366e9b02e11cff1e0f89513330b6
yrberrytea <yrberrytea@gmail.com>
2022-03-11 13:02:26 +0900

    few fixes

--- competitive-programming/ 競プロ典型90問/36.py
+++ competitive-programming/ 競プロ典型90問/36.py
@@ -1,23 +1,30 @@
-n, q = map(int, input().split())
-manhattan = [""] * n
+import sys
 
-for i in range(n):
-    x, y = map(int, input().split())
-    manhattan[i] = (x + y, x - y)
 
-compair = [0] * 4
+def main():
+    n, q = map(int, sys.stdin.readline().split())
+    manhattan = [""] * n
 
-for i, j in manhattan:
-    compair[0] = max(compair[0], i)
-    compair[1] = min(compair[1], i)
-    compair[2] = max(compair[2], j)
-    compair[3] = min(compair[3], j)
+    for i in range(n):
+        x, y = map(int, sys.stdin.readline().split())
+        manhattan[i] = (x + y, x - y)
 
-for _ in range(q):
-    qi = int(input())
-    d1 = abs(compair[0] - manhattan[qi - 1][0])
-    d2 = abs(compair[1] - manhattan[qi - 1][0])
-    d3 = abs(compair[2] - manhattan[qi - 1][1])
-    d4 = abs(compair[3] - manhattan[qi - 1][1])
-    ans = max((d1, d2, d3, d4))
-    print(ans)
+    compair = [0] * 4
+
+    for i, j in manhattan:
+        compair[0] = max(compair[0], i)
+        compair[1] = min(compair[1], i)
+        compair[2] = max(compair[2], j)
+        compair[3] = min(compair[3], j)
+
+    for _ in range(q):
+        qi = int(input())
+        d1 = abs(compair[0] - manhattan[qi - 1][0])
+        d2 = abs(compair[1] - manhattan[qi - 1][0])
+        d3 = abs(compair[2] - manhattan[qi - 1][1])
+        d4 = abs(compair[3] - manhattan[qi - 1][1])
+        ans = max((d1, d2, d3, d4))
+        print(ans)
+
+
+main()
--- competitive-programming/template/template.py
+++ competitive-programming/template/template.py
@@ -1,17 +1,27 @@
-# input
+# 入力
+
+# 実行時間がギリギリな場合はsys.stdin.readline()を使う
+# input()より速い
+import sys
 
 # foobar
 s = input()
+s = sys.stdin.readline()
 # 1
 n = int(input())
+n = int(sys.stdin.readline())
 # 1 2
 a, b = map(int, input().split())
+a, b = map(int, sys.stdin.readline().split())
 # a0 a1 a2 a3 a4
 a = list(map(int, input().split()))
+a = list(map(int, sys.stdin.readline().split()))
 # a00 a01 a02 a03 a04
 # a10 a11 a12 a13 a14
 # a20 a21 a22 a23 a24
 a = [list(map(int, input().split())) for i in range(n)]
+a = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+
 
 # s[n] 初期値:0
 backet = [0] * n
