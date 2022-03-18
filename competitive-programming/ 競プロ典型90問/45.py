d81f529a3065c8d08902b9badaf8a3f96755de4c
yrberrytea <yrberrytea@gmail.com>
2022-03-18 19:38:19 +0900

    solve question

--- competitive-programming/ 競プロ典型90問/45.py
+++ competitive-programming/ 競プロ典型90問/45.py
@@ -1,9 +1,8 @@
 # まだワーストケースが通らない
+
+# 入力
 n, k = map(int, input().split())
-dots = [""] * n
-for i in range(n):
-    x, y = map(int, input().split())
-    dots[i] = (x, y)
+dots = [tuple(map(int, input().split())) for i in range(n)]
 
 # 二点間距離
 d2 = [[0] * n for i in range(n)]
@@ -11,15 +10,20 @@ for i in range(n):
     for j in range(n):
         d2[i][j] = abs(dots[i][0] - dots[j][0]) ** 2 + abs(dots[i][1] - dots[j][1]) ** 2
 
+# 部分集合の要素のインデックス列挙
+subsetsIndexes = [[] for i in range(2 ** n)]
+for i in range(1, 2 ** n):
+    for j in range(n):
+        if (i >> j) & 1:
+            subsetsIndexes[i].append(j)
+
 # 部分集合の二点間距離最大値
 dist = [0] * 2 ** n
 for i in range(1, 2 ** n):
     dj = 0
-    for j in range(n):
-        if (i >> j) & 1:
-            for m in range(n):
-                if (i >> m) & 1:
-                    dj = max(dj, d2[j][m])
+    for j in subsetsIndexes[i]:
+        for m in subsetsIndexes[i]:
+            dj = max(dj, d2[j][m])
     dist[i] = dj
 
 # 部分集合の列挙
@@ -31,15 +35,14 @@ for i in range(1, 2 ** n):
         v = (v - 1) & i
 
 # bitDP
-INF = float('inf')
+INF = 10 ** 20
 dp = [[INF] * (k + 1) for i in range(2 ** n)]
 dp[0][0] = 0
 for j in range(1, k + 1):
     for i in range(1, 2 ** n):
         minDist = INF
         for b in subsets[i]:
-            d = max(dp[i - b][j - 1], dist[b])
-            minDist = min(minDist, d)
+            minDist = min(minDist, max(dp[i - b][j - 1], dist[b]))
         dp[i][j] = minDist
 
 print(dp[2 ** n - 1][k])
